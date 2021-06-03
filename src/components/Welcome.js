77a984199a09157b181ae653f26c9b48dc1c8b1e
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-06-03 13:20:49 -0400

    removed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 constructor() {
     super()
-    this.state={
+    this.state = {
         message: 'Welcome User'
     }
 }
 
 const WelcomeUser = () => {
-    return(
+    return (
         <div>
             <h1>
                 {this.state.message}
             </h1>
             <button type="button" class="btn btn-danger">Sign out</button>
-            <button type="button" class="btn btn-danger">Sign out</button>
+        //Removed
         </div>
     )
 }
