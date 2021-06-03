e3a85006c59bc20cf05c6e832fae4e95b970d809
Gabriel Gualteros <Roblox4@gmail.com>
2021-06-03 15:09:15 -0400

    added and removed import from app.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import './App.css';
 
 import Header from './components/Header';
 import Landing from './components/Landing';
-<<<<<<< HEAD
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import Login from './components/Login'
 import AccountHoldersList from './components/AccountHolders'
-=======
-import Footer from './components/footer';
-import Welcome from './components/Welcome'
->>>>>>> f2aeeabceab6d7064a9a0f0ea03b7433a90a08d3
 
 
 function App() {
@@ -33,17 +28,10 @@ function App() {
                 </Switch>
 
                 <Footer />
-<<<<<<< HEAD
             </Router>
         </div>
 
     );
-=======
-                
-            </div>
-        );
-    }
->>>>>>> f2aeeabceab6d7064a9a0f0ea03b7433a90a08d3
 }
 
 export default App
